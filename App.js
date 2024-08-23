f2009f8e62f2f4cf17d2dce7c631b5c2e56d2442
jdmm99 <yedamomo@gmail.com>
2024-08-23 17:13:26 -0500

    Finalización primer ejercicio mobile

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box1,{justifyContent: 'flex-start'} ]}/>
+      <View style={[styles.box2]} />
+      <View style={styles.box3}>
+         <View style={[styles.box3_1]} />
+         <View style={[styles.box3_1]} />
+         <View style={[styles.box3_1]} />
+      </View>
+      <View style={[styles.box4]} />
     </View>
   );
 }
@@ -14,7 +19,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  box1:{
+    width: '90%',
+    height: 100,
+    backgroundColor:'#169',
+  },
+  box2:{
+    width: '90%',
+    height: 300,
+    backgroundColor:'#500'
+  },
+  box3:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+
+  box3_1:{
+    backgroundColor: '#700',
+    width: 100,
+    height: 100,
+  },
+  box4:{
+    width: '90%',
+    height: 100,
+    backgroundColor:'#698',
+  }
 });
